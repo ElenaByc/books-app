d0f27c857574f7892ffbb52db1c3866c0cb2c5cf
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-05-09 15:59:23 -0700

    feat: seed db categories based on NYT Best Sellers Lists

--- seed_database.py
+++ seed_database.py
@@ -10,6 +10,10 @@ import crud
 import model
 import server
 
+
+NYT_API_KEY = os.environ['NYT_KEY']
+
+
 # re-create a database
 os.system('dropdb books')
 os.system('createdb books')
@@ -18,8 +22,46 @@ os.system('createdb books')
 model.connect_to_db(server.app)
 model.db.create_all()
 
+
 # get all Best Sellers Lists from New York Times Books API
 # and create subjects data based on this lists
+url = 'https://api.nytimes.com/svc/books/v3/lists/names.json'
+payload = {}
+payload['api-key'] = NYT_API_KEY
+
+res = requests.get(url, params=payload)
+print(res)
+print(res.url)
+data = res.json()
+total_items = data['num_results']
+print(total_items)
+if total_items != 0:
+    subjects = data['results']
+else:
+    subjects = []
+
+subjects_in_db = []
+
+for subject in subjects:
+    # Get the display_name and list_name_encoded, and
+    display_name = subject['display_name']
+    list_name_encoded = subject['list_name_encoded']
+    # Get newest_published_date and convert it to a datetime object
+    newest_published_date = datetime.strptime(
+        subject['newest_published_date'], "%Y-%m-%d")
+    published_year = newest_published_date.year
+    current_year = datetime.now().year
+    # print(type(published_year), published_year, current_year)
+    # Select only recently published lists, and not audio
+    if published_year == current_year and not 'audio' in list_name_encoded:
+        # Create a subject and append it to subjects_in_db
+        db_subject = crud.create_subject(
+            subject=display_name, nyt_list_name=list_name_encoded)
+        subjects_in_db.append(db_subject)
+
+# Add all subjects to the SQLAlchemy session and commit them to db
+model.db.session.add_all(subjects_in_db)
+model.db.session.commit()
 
 
 # For each list/subject from New York Times Best Sellers Lists get books
