e705b599962925232eef95f0153c03605cd30ccc
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-06-02 00:05:07 -0700

    feat: add Bootstrap, add modal for book details

--- static/js/BookCard.jsx
+++ static/js/BookCard.jsx
@@ -1,4 +1,10 @@
 const BookCard = ({ book, shelf, handleLeftBtn, handleRightBtn, setMsg }) => {
+
+  const [show, setShow] = React.useState(false);
+
+  const handleCloseDetails = () => setShow(false);
+  const handleShowDetails = () => setShow(true);
+
   let author_data;
   if (book.authors.length > 1) {
     if (book.authors.length === 2) {
@@ -19,17 +25,58 @@ const BookCard = ({ book, shelf, handleLeftBtn, handleRightBtn, setMsg }) => {
 
   return (
     <div className="card">
-      <div className="card_img">
-        {(book.covers.length > 0)
-          ? <img src={book.covers[0]} alt="book cover" />
-          : <img src="/static/img/no-cover.jpg" alt="book cover is not available" />
-        }
+      {/* <Example book = {book}/> */}
+      <div className="card_popup" onClick={handleShowDetails}>
+        <div className="card_img" title="View book details">
+          {(book.covers.length > 0)
+            ? <img src={book.covers[0]} alt="book cover" />
+            : <img src="/static/img/no-cover.jpg" alt="book cover is not available" />
+          }
+        </div>
+        <div className="card_content">
+          <h3>{book.title}</h3>
+          <h4>{author_data}</h4>
+        </div>
       </div>
-      <div className="card_content">
-        <h3><a href={`/books/${book.book_id}`}>{book.title}</a></h3>
-        <h4>{author_data}</h4>
-      </div>
-      {shelf !== "" &&
+      <ReactBootstrap.Modal
+        show={show}
+        onHide={handleCloseDetails}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <ReactBootstrap.Modal.Header closeButton>
+          <h2>Book Title: {book.title}</h2>
+        </ReactBootstrap.Modal.Header>
+        <ReactBootstrap.Modal.Body>
+          <div className="book_details">
+            {(book.covers.length > 0)
+              ? <img className="book_details__cover" src={book.covers[0]} alt="book cover" />
+              : <img src="/static/img/no-cover.jpg" alt="book cover is not available" />
+            }
+            <div class="book_details__content">
+              <h4>{author_data}</h4>
+              {book.contributor_note != "" && <h4>{book.contributor_note}</h4>}
+              <h4>ISBN10 = {book.primary_isbn10}</h4>
+              <h4>ISBN13 = {book.primary_isbn13}</h4>
+              <h4>NEW YORK TIMES BESTSELLER in {book.lists.length}&nbsp;
+                {book.lists.length > 1 ? <span>lists</span> : <span>list</span>}
+              </h4>
+              <ul>
+                {book.lists.map((list_name) => <li key={list_name}>{list_name}</li>)}
+              </ul>
+              <h4>Categories:</h4>
+              <ul>
+                {book.categories.map((category_name) => <li key={category_name}>{category_name}</li>)}
+              </ul>
+              <h4>Description:</h4>
+              <div dangerouslySetInnerHTML={{ __html: book.description }} />
+            </div>
+          </div>
+        </ReactBootstrap.Modal.Body>
+      </ReactBootstrap.Modal>
+      {
+        shelf !== "" &&
         <CardButtons
           shelf={shelf}
           book={book}
@@ -38,6 +85,6 @@ const BookCard = ({ book, shelf, handleLeftBtn, handleRightBtn, setMsg }) => {
           setMsg={setMsg}
         />
       }
-    </div>
+    </div >
   )
 }
