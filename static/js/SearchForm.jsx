e31cd5476425bb7449950a68ea14fcefc9528038
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-06-05 18:58:37 -0700

    style: add styles to search form, remove unnecessary HTML code and script

--- static/js/SearchForm.jsx
+++ static/js/SearchForm.jsx
@@ -114,66 +114,65 @@ const SearchForm = () => {
 
   return (
     <>
-      <h3 className="search-header">Pick Me a Book</h3>
+      <div className="search-container">
+        <h3 className="search-header">Pick Me a Book</h3>
 
-      <form className="search-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="options">Choose books</label>
-          <select defaultValue={"DEFAULT"} onChange={handleOptionSelect}>
-            <option value="DEFAULT" disabled> -- select an option -- </option>
-            <option value="author">by author</option>
-            <option value="title">by title</option>
-            <option value="category">by category</option>
-            <option value="list">by NYT best sellers list</option>
-            {isLoggedIn &&
-              <option value="favorites">based on my Favorites</option>
-            }
-          </select>
-        </div>
-        {option === "author" &&
+        <form className="search-form" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="author">Author</label>
-            <input id="author" type="text" onChange={handleAuthorChange} />
-          </div>
-        }
-        {option === "title" &&
-          <div>
-            <label htmlFor="title">Title</label>
-            <input id="title" type="text" onChange={handleTitleChange} />
-          </div>
-        }
-        {option === "category" &&
-          <div>
-            <label htmlFor="category">Category</label>
-            <input id="category" type="text" placeholder="example: Fantasy" onChange={handleCategoryChange} />
-          </div>
-        }
-        {option === "list" &&
-          <div>
-            <label htmlFor="list">Best Sellers List</label>
-            <select id="list-select" defaultValue={"DEFAULT"} onChange={handleListSelect}>
-              <option value="DEFAULT" disabled> -- select a list -- </option>
-              {listsArray.length > 0
-                ? listsArray.map((l) => <option value={l.list_id} key={l.list_id}>{l.list_name}</option>)
-                : <option disabled>Loading...</option>
+            <label htmlFor="options">Choose books</label>
+            <select defaultValue={"DEFAULT"} onChange={handleOptionSelect}>
+              <option value="DEFAULT" disabled> -- select an option -- </option>
+              <option value="author">by author</option>
+              <option value="title">by title</option>
+              <option value="category">by category</option>
+              <option value="list">by NYT best sellers list</option>
+              {isLoggedIn &&
+                <option value="favorites">based on my Favorites</option>
               }
             </select>
           </div>
-        }
+          {option === "author" &&
+            <div>
+              <label htmlFor="author">Author</label>
+              <input id="author" type="text" onChange={handleAuthorChange} />
+            </div>
+          }
+          {option === "title" &&
+            <div>
+              <label htmlFor="title">Title</label>
+              <input id="title" type="text" onChange={handleTitleChange} />
+            </div>
+          }
+          {option === "category" &&
+            <div>
+              <label htmlFor="category">Category</label>
+              <input id="category" type="text" placeholder="example: Fantasy" onChange={handleCategoryChange} />
+            </div>
+          }
+          {option === "list" &&
+            <div>
+              <label htmlFor="list">Best Sellers List</label>
+              <select id="list-select" defaultValue={"DEFAULT"} onChange={handleListSelect}>
+                <option value="DEFAULT" disabled> -- select a list -- </option>
+                {listsArray.length > 0
+                  ? listsArray.map((l) => <option value={l.list_id} key={l.list_id}>{l.list_name}</option>)
+                  : <option disabled>Loading...</option>
+                }
+              </select>
+            </div>
+          }
 
 
-        {isLoggedIn &&
-          <div>
-            <input type="checkbox" id="checkmark" onChange={handleCheckmarkChange} defaultChecked />
-            <label htmlFor="checkmark" className="label">Do not recommend me books that already on my bookshelf</label>
-          </div>
-        }
+          {isLoggedIn &&
+            <div>
+              <input type="checkbox" id="checkmark" onChange={handleCheckmarkChange} defaultChecked />
+              <label htmlFor="checkmark" className="label">Do not recommend me books that already on my bookshelf</label>
+            </div>
+          }
 
-        <input type="submit" value="Get Recommendations!" disabled={!searchQuery} />
-      </form>
-
-      <br></br>
-      <br></br>
+          <input type="submit" value="Get Recommendations!" disabled={!searchQuery} />
+        </form>
+      </div>
       {msg !== "" && <MessageBox msg={msg} setMsg={setMsg} />}
       {searchStatus !== "" &&
         <BooksContainer
